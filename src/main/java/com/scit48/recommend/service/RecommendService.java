4ab2ce5e32f24c5ed7bd9adf437c1b13f1062800
user <gimhansig80@gmail.com>
2026-01-20 17:38:52 +0900

    First Commit

---
+++ src/main/java/com/scit48/recommend/service/RecommendService.java
@@ -0,0 +1,13 @@
+package com.scit48.recommend.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+public class RecommendService {
+}
