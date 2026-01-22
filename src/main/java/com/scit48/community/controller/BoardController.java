d98e62a54b16ba2b9ed6fdb6a4ecf68591a21a07
unknown <cky>
2026-01-22 14:22:24 +0900

    save

--- src/main/java/com/scit48/community/controller/BoardController.java
+++ src/main/java/com/scit48/community/controller/BoardController.java
@@ -2,11 +2,18 @@ package com.scit48.community.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("board")
 public class BoardController {
+	
+	@GetMapping("write")
+	public String write () {
+		
+		return "boardWrite";
+	}
 
 }
